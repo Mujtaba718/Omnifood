95f7c609940edd6edb42a4c6d5bef693c49e475e
Mujtaba Iqbal <mujtaba.iq@gmail.com>
2018-09-18 19:44:03 -0400

    Created docs folder for production.

---
+++ docs/js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+	$('.section-features').waypoint(function(direction){
+		if(direction == "down") {
+			$('nav').addClass('sticky')
+		} else {
+			$('nav').removeClass('sticky')
+		}
+	}, {
+		offset: '60px;'
+	});
+
+	$('.js--scroll-to-plans').click(function() {
+		$('html, body').animate({scrollTop: $('.section-plans').offset().top}, 1000)
+	})
+
+	$('.js--scroll-to-start').click(function() {
+		$('html, body').animate({scrollTop: $('.section-features').offset().top}, 1000)
+	})
+
+	$(function() {
+	  $('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
+	 
+	$('.js--wp-1').waypoint(function(direction){
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-2').waypoint(function(direction){
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-3').waypoint(function(direction){
+		$('.js--wp-3').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-4').waypoint(function(direction){
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
+
+	// Mobile Navigation
+	$('.js--nav-icon').click(function(){
+		let nav = $('.js--main-nav');
+		let icon = $('.js--nav-icon ion-icon');
+
+		if (icon[0].name == "menu") {
+			icon[0].name = "close"
+		} else {
+			icon[0].name = "menu"
+		}
+
+		nav.slideToggle(200)
+	})
+
+})
